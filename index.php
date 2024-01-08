0c964d1bd3ca840032bb103c234a3525d33dd64d
GiovanniCadoni <giocadoni19@gmail.com>
2024-01-08 19:10:37 +0100

    add: class, constructor and function

--- index.php
+++ index.php
@@ -1,5 +1,38 @@
 <?php
+class Movie {
+    public $name;
+    public $year;
+    public $language;
+    public $view;
+    public $status;
 
+    function __construct($_name, $_year, $_language, $_view) {
+        $this->name = $_name;
+        $this->year = $_year;
+        $this->language = $_language;
+        $this->view = strtolower($_view);
+        $this->setView(strtolower($_view));
+    }
+
+    public function setView($view) {
+        if ($view == 'yes') {
+            $this->status = 'View';
+        } else if ($view == 'no') {
+            $this->status = 'Not View';
+        } else {
+            $this->status = 'error';
+        }
+    }
+}
+
+$movies = [
+    new Movie('Avatar', '2009', 'english', 'Yes'),
+    new Movie('Pulp fiction', '1994', 'english', 'no'),
+    new Movie('Oppenheimer', '2023', 'english', 'si'),
+    new Movie('The Marvels', '2023', 'english', 'yes'),
+    new Movie('Pacific Rim', '2013', 'english', 'Yes'),
+];
+//var_dump($movies);
 
 ?>
 
@@ -11,7 +44,26 @@
         <title>Movie</title>
     </head>
     <body>
-        
-        
+        <h2>
+            Movies:
+        </h2>
+        <ul>
+            <?php foreach($movies as $movie) { ?>
+                <li>
+                    <h3>
+                        Name: <?php echo $movie->name ?>
+                    </h3>
+                    <p>
+                        Year: <?php echo $movie->year ?>
+                    </p>
+                    <p>
+                        Language: <?php echo $movie->language ?>
+                    </p>
+                    <i>
+                        Status: <?php echo $movie->status ?>
+                    </i>
+                </li>
+            <?php } ?>
+        </ul>
     </body>
 </html>
